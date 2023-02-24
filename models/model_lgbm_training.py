1b71f3e955ac1780f7eb36625a039144ab4f2eef
eugenos-programos <elena.samchuk.73@mail.ru>
2023-02-24 21:51:53 +0300

    Train LGBM model

---
+++ models/model_lgbm_training.py
@@ -0,0 +1,35 @@
+from lightgbm import LGBMClassifier
+import mlflow as mlf
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+
+
+with mlf.start_run(tags={"version" : "1.0.0", "type" : "boosting"}, run_name='lightgbm-1.0.0-run') as run:
+    
+    params = {
+        'learning_rate' : 0.08,
+        'n_estimators' : 400
+    }
+    
+    mlf.log_params(params)
+
+    data_file_name = 'preproc_train.csv'
+    vectorizer_file_name = 'vectorizer.pkl'
+
+    mlf.models.Model()
+
+    data = pd.read_csv(data_file_name).dropna()
+    with open(vectorizer_file_name, 'rb') as vecfile:
+        vectorizer = pickle.load(vecfile)
+    mlf.log_param('vectorizer', vectorizer)
+    encoded_data = vectorizer.transform(data.title)
+
+    model = LGBMClassifier(**params)
+    model.fit(encoded_data, data.label)
+
+    score = model.score(encoded_data, data.label)
+    mlf.log_metric("accuracy", score)
+    signature = mlf.models.signature.infer_signature(encoded_data, data.label)
+
+    mlf.lightgbm.save_model(model, "LGBMClassifier", signature=signature, input_example=encoded_data)
