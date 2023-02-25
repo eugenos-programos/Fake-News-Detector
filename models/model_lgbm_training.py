441e2974d2bafcf3df7a44cd86e65c44ebe0d18a
eugenos-programos <elena.samchuk.73@mail.ru>
2023-02-25 21:27:49 +0300

    Add config yaml file

--- models/model_lgbm_training.py
+++ models/model_lgbm_training.py
@@ -2,10 +2,10 @@ from lightgbm import LGBMClassifier
 import mlflow as mlf
 import pandas as pd
 import pickle
-from sklearn.metrics import accuracy_score
+import yaml
 
 
-with mlf.start_run(tags={"version" : "1.0.0", "type" : "boosting"}, run_name='lightgbm-1.0.0-run') as run:
+with mlf.start_run(tags={"version" : "2.0.0", "type" : "boosting"}, run_name='lightgbm-2.0.0-run') as run:
     
     params = {
         'learning_rate' : 0.08,
@@ -14,19 +14,25 @@ with mlf.start_run(tags={"version" : "1.0.0", "type" : "boosting"}, run_name='li
     
     mlf.log_params(params)
 
-    data_file_name = 'preproc_train.csv'
-    vectorizer_file_name = 'vectorizer.pkl'
+    with open("models_config.yml") as confile:
+        confs = yaml.safe_load(confile)
 
-    data = pd.read_csv(data_file_name).dropna()
+    train_file_name = confs['train_data_file']
+    val_file_name = confs['val_data_file']
+    vectorizer_file_name = confs['vectorizer_file']
+
+    train_data = pd.read_csv(train_file_name).dropna()
+    val_data = pd.read_csv(val_file_name).dropna()
     with open(vectorizer_file_name, 'rb') as vecfile:
         vectorizer = pickle.load(vecfile)
-    encoded_data = vectorizer.transform(data.title)
+    encoded_train_data = vectorizer.transform(train_data.title)
+    encoded_val_data = vectorizer.transform(val_data.title)
 
     model = LGBMClassifier(**params)
-    model.fit(encoded_data, data.label)
+    model.fit(encoded_train_data, train_data.label)
 
-    score = model.score(encoded_data, data.label)
-    mlf.log_metric("accuracy", score)
-    signature = mlf.models.signature.infer_signature(encoded_data, data.label)
+    score = model.score(encoded_val_data, val_data.label)
+    mlf.log_metric("val_accuracy", score)
+    signature = mlf.models.signature.infer_signature(encoded_val_data, val_data.label)
 
-    mlf.lightgbm.log_model(model, "LGBMClassifier", signature=signature, input_example=encoded_data, registered_model_name="LGBMClassifier")
+    mlf.lightgbm.log_model(model, "lgbm_model", signature=signature, input_example=encoded_val_data, registered_model_name="LGBMClassifier")
