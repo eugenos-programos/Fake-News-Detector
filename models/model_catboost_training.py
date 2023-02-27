db46f8768350985bc51b2ea62f15eac5a558ac43
eugenos-programos <elena.samchuk.73@mail.ru>
2023-02-27 14:44:09 +0300

    Add saving function in mlflow format

--- models/model_catboost_training.py
+++ models/model_catboost_training.py
@@ -6,34 +6,49 @@ import yaml
 import os
 
 
-with mlf.start_run(tags={"version" : "1.0.0", "type" : "boosting"}, run_name='catboost-1.0.0-run') as run:
-    
-    params = {
-        'iterations': 1000,
-        'learning_rate': 0.05, 
-        'l2_leaf_reg' : 5
-    }
-    mlf.log_params(params)
+class CatBoostModel():
+    def __init__(self) -> None:
+        pass
 
-    with open("models_config.yml") as confile:
-        confs = yaml.safe_load(confile)
+    def train_and_log(self):
+        with mlf.start_run(tags={"version" : "1.0.0", "type" : "boosting"}, run_name='catboost-1.0.0-run') as run:
+            
+            params = {
+                'iterations': 1000,
+                'learning_rate': 0.05, 
+                'l2_leaf_reg' : 5
+            }
+            mlf.log_params(params)
 
-    train_file_name = confs['train_data_file']
-    val_file_name = confs['val_data_file']
-    vectorizer_file_name = confs['vectorizer_file']
+            with open("models_config.yml") as confile:
+                confs = yaml.safe_load(confile)
 
-    train_data = pd.read_csv(os.path.join('..', train_file_name)).dropna()
-    val_data = pd.read_csv(os.path.join('..', val_file_name)).dropna()
-    with open(os.path.join('..', vectorizer_file_name, 'rb')) as vecfile:
-        vectorizer = pickle.load(vecfile)
-    encoded_train_data = vectorizer.transform(train_data.title)
-    encoded_val_data = vectorizer.transform(val_data.title)
+            train_file_name = confs['train_data_file']
+            val_file_name = confs['val_data_file']
+            vectorizer_file_name = confs['vectorizer_file']
 
-    model = CatBoostClassifier(**params)
-    model.fit(encoded_train_data, train_data.label, eval_set=(encoded_val_data, val_data.label), use_best_model=True)
+            train_data = pd.read_csv(os.path.join('..', train_file_name)).dropna()
+            val_data = pd.read_csv(os.path.join('..', val_file_name)).dropna()
+            with open(os.path.join('..', vectorizer_file_name, 'rb')) as vecfile:
+                vectorizer = pickle.load(vecfile)
+            encoded_train_data = vectorizer.transform(train_data.title)
+            encoded_val_data = vectorizer.transform(val_data.title)
 
-    score = model.score(encoded_val_data, val_data.label)
-    mlf.log_metric("val_accuracy", score)
-    signature = mlf.models.signature.infer_signature(encoded_val_data, val_data.label)
+            model = CatBoostClassifier(**params)
+            model.fit(encoded_train_data, train_data.label, eval_set=(encoded_val_data, val_data.label), use_best_model=True)
 
-    mlf.catboost.log_model(model, "catboost_model", signature=signature, input_example=encoded_val_data, registered_model_name="CatBoostClassifier")
+            score = model.score(encoded_val_data, val_data.label)
+            mlf.log_metric("val_accuracy", score)
+            signature = mlf.models.signature.infer_signature(encoded_val_data, val_data.label)
+
+            mlf.catboost.log_model(model, "catboost_model", signature=signature, input_example=encoded_val_data, registered_model_name="CatBoostClassifier")
+
+    def save_model(self):
+        model = mlf.catboost.load_model('models:/CatBoostClassifier/latest')
+        mlf.catboost.save_model(model, './CatBoostClassifier')
+        
+
+if __name__ == '__main__':
+    model = CatBoostModel()
+    model.train_and_log()
+    model.save_model()
--- models/model_lgbm_training.py
+++ models/model_lgbm_training.py
@@ -6,34 +6,49 @@ import yaml
 import os
 
 
-with mlf.start_run(tags={"version" : "2.0.0", "type" : "boosting"}, run_name='lightgbm-2.0.0-run') as run:
-    
-    params = {
-        'learning_rate' : 0.08,
-        'n_estimators' : 400
-    }
-    
-    mlf.log_params(params)
+class LGBMmodel():
+    def __init__(self) -> None:
+        pass
 
-    with open("models_config.yml") as confile:
-        confs = yaml.safe_load(confile)
+    def train_and_log(self):
+        with mlf.start_run(tags={"version" : "2.0.0", "type" : "boosting"}, run_name='lightgbm-2.0.0-run') as run:
+            
+            params = {
+                'learning_rate' : 0.08,
+                'n_estimators' : 400
+            }
+            
+            mlf.log_params(params)
 
-    train_file_name = confs['train_data_file']
-    val_file_name = confs['val_data_file']
-    vectorizer_file_name = confs['vectorizer_file']
+            with open("models_config.yml") as confile:
+                confs = yaml.safe_load(confile)
 
-    train_data = pd.read_csv(os.path.join('..', train_file_name)).dropna()
-    val_data = pd.read_csv(os.path.join('..', val_file_name)).dropna()
-    with open(os.path.join('..', val_file_name), 'rb') as vecfile:
-        vectorizer = pickle.load(vecfile)
-    encoded_train_data = vectorizer.transform(train_data.title)
-    encoded_val_data = vectorizer.transform(val_data.title)
+            train_file_name = confs['train_data_file']
+            val_file_name = confs['val_data_file']
+            vectorizer_file_name = confs['vectorizer_file']
 
-    model = LGBMClassifier(**params)
-    model.fit(encoded_train_data, train_data.label)
+            train_data = pd.read_csv(os.path.join('..', train_file_name)).dropna()
+            val_data = pd.read_csv(os.path.join('..', val_file_name)).dropna()
+            with open(os.path.join('..', val_file_name), 'rb') as vecfile:
+                vectorizer = pickle.load(vecfile)
+            encoded_train_data = vectorizer.transform(train_data.title)
+            encoded_val_data = vectorizer.transform(val_data.title)
 
-    score = model.score(encoded_val_data, val_data.label)
-    mlf.log_metric("val_accuracy", score)
-    signature = mlf.models.signature.infer_signature(encoded_val_data, val_data.label)
+            model = LGBMClassifier(**params)
+            model.fit(encoded_train_data, train_data.label)
 
-    mlf.lightgbm.log_model(model, "lgbm_model", signature=signature, input_example=encoded_val_data, registered_model_name="LGBMClassifier")
+            score = model.score(encoded_val_data, val_data.label)
+            mlf.log_metric("val_accuracy", score)
+            signature = mlf.models.signature.infer_signature(encoded_val_data, val_data.label)
+
+            mlf.lightgbm.log_model(model, "lgbm_model", signature=signature, input_example=encoded_val_data, registered_model_name="LGBMClassifier")
+
+    def save_model(self):
+        model = mlf.lightgbm.load_model('models:/LGBMClassifier/latest')
+        mlf.lightgbm.save_model(model, './LGBMClassifier')
+        
+
+if __name__ == '__main__':
+    model = LGBMmodel()
+    model.train_and_log()
+    model.save_model()
